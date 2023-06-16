d2c05cbca4da2f1cfd368c9fe451fd37aadc9044
Anthony-svg <anthony.melendez07.am@gmail.com>
2023-06-16 10:39:15 -0500

    cambios servicios

--- models/InsertarTarea.php
+++ models/InsertarTarea.php
@@ -8,14 +8,16 @@
     $desTar= $_POST['descripcion'];
     $FeIn= $_POST['fecIniTar'];
     $FeFi= $_POST['fecFinTar'];
+    $est= $_POST['Estado'];
 
-    $sql = "INSERT INTO tarea(idTar, nomTar, desTar, fecIniTar, fecFinTar) VALUES (:idT, :nomT, :desT, :feIn, :feFi )";
+    $sql = "INSERT INTO tarea(idTar, nomTar, desTar, fecIniTar, fecFinTar,estTar) VALUES (:idT, :nomT, :desT, :feIn, :feFi ,:est )";
     $sql2 = $pdo->prepare($sql);
     $sql2->bindParam(':idT',$id,PDO::PARAM_INT,5);
     $sql2->bindParam(':nomT',$nomTar,PDO::PARAM_STR,25);
     $sql2->bindParam(':desT',$desTar,PDO::PARAM_STR,25);
     $sql2->bindParam(':feIn',$FeIn,PDO::PARAM_STR,25);
     $sql2->bindParam(':feFi',$FeFi,PDO::PARAM_STR,25);
+    $sql2->bindParam(':est',$est,PDO::PARAM_STR,25);
 
     $sms="";
 
