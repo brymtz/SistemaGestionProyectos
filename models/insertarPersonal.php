d0d28c88ca88a6977e4cb73b2e38c82c677ec3b4
Anthony-svg <anthony.melendez07.am@gmail.com>
2023-06-16 12:54:12 -0500

    cambio servicio contraseña

--- models/insertarPersonal.php
+++ models/insertarPersonal.php
@@ -4,14 +4,16 @@
     include "conexion.php";
 
     $user= $_POST['id'];
+    $passPer= $_POST['password'];
     $cedPer= $_POST['cedula'];
     $nomPer= $_POST['nombre'];
     $rolPer= $_POST['rol'];
     $disPer= $_POST['disponibilidad'];
 
-    $sql = "INSERT INTO personal(userPer, cedPer, nomPer, rolPer, disPer) VALUES (:user, :cedPer, :nomPer, :rolPer, :disPer )";
+    $sql = "INSERT INTO personal(userPer, passPer, cedPer, nomPer, rolPer, disPer) VALUES (:user, :passPer, :cedPer, :nomPer, :rolPer, :disPer )";
     $sql2 = $pdo->prepare($sql);
     $sql2->bindParam(':user',$user,PDO::PARAM_STR,5);
+    $sql2->bindParam(':passPer',$passPer,PDO::PARAM_STR,25);
     $sql2->bindParam(':cedPer',$cedPer,PDO::PARAM_STR,25);
     $sql2->bindParam(':nomPer',$nomPer,PDO::PARAM_STR,25);
     $sql2->bindParam(':rolPer',$rolPer,PDO::PARAM_STR,25);
