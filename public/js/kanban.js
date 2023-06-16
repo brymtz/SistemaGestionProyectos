468baed9dc436b72e2e854c776ef8f511efe1e81
Paulo Martinez <paulomartinez1999@gmail.com>
2023-06-16 12:04:07 -0500

    algo

--- public/js/kanban.js
+++ public/js/kanban.js
@@ -1,8 +1,1 @@
 const p = document.getElementById("todo-lane");
-/*
-fetch('../models/consultaTareas.php')
-    .then(response => response.json() )
-    .then(data => {
-        console.log(data);
-        p.innerHTML=data;
-} )*/
